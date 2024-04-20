e4752df291d1c14b24c4f8e85dacd2ec37ed8a10
xStef23 <trstefy@gmail.com>
2024-04-20 14:33:08 +0100

    small edits

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ export default function About() {
       <div className="mb-[1rem] p-[2rem] rounded-2xl bg-[#EDF1FA] dark:bg-[#1D2333]  p-4 shadow-md shadow-black dark:shadow-[#1d4ed8]  md:w-3/5">
         <h1 className="text-2xl font-bold font-header">WHO ARE YOU?</h1>
         <p className="font-semibold font-text">
-          Hello there! I'm x, a web developer who grew up with the internet. I
+          Hello there! I am x, a web developer who grew up with the internet. I
           love crafting things that people can use and enjoy online.
         </p>
       </div>
@@ -18,9 +18,8 @@ export default function About() {
         <p className="mb-[1rem] font-semibold font-text">
           The internet is my playground, and I love building websites and apps
           that are born and live there. I specialize in creating the visual side
-          of things (front-end development) and making sure it's easy and
-          enjoyable to use (user interface and experience design), but I'm also
-          well-versed in some back-end functionalities to make sure everything
+          of things and making sure it's easy and enjoyable to use, but I am
+          also versed in some back-end functionalities to make sure everything
           runs smoothly.
         </p>
         <p className="font-semibold font-text">
@@ -34,10 +33,10 @@ export default function About() {
           Challenges I can rise up to.
         </p>
         <p className="mb-[1rem] font-semibold font-text">
-          I want to do good in the world. I don’t feel I have yet.
+          I want to do good in the world. I do not feel I have yet.
         </p>
         <p className="font-semibold font-text">
-          I'm passionate about all kinds of projects, but the ones that truly
+          I am passionate about all kinds of projects, but the ones that truly
           energize me are those that make a positive impact. This passion fuels
           my love for creating software that helps others by making their lives
           easier, apps that brighten your day, and websites that promote equal
@@ -52,14 +51,13 @@ export default function About() {
         </p>
         <p className="mb-[1rem] font-semibold font-text">
           Online, I hang out on various platforms to get inspiration for my app
-          ideas, watch anime and sports (we can talk Formula 1 and football) ,
-          go down rabbit holes of all sorts, and learn how to build things for
-          the web.
+          ideas, watch anime and sports, mostly Formula 1 and football, go down
+          rabbit holes of all sorts, and learn how to build things for the web.
         </p>
         <p className="font-semibold font-text">
-          Offline, I'm into lifting weights and calisthenics. For quite a long
-          time, I've been working on becoming a master chef (of course, in my
-          own kitchen). I enjoy a good walk in nature and petting my cats. Video
+          Offline, I am into lifting weights and calisthenics. For quite a long
+          time, I've been working on becoming a master chef, of course, in my
+          own kitchen. I enjoy a good walk in nature and petting my cats. Video
           games, I like those too.
         </p>
       </div>
